7b9d06380fce658be8b0a1ca7d936243eb61be1a
Erick Ramírez <erramire@microsoft.com>
2024-11-06 14:15:02 -0600

    Implement Maintenance Copilot chat functionality and enhance maintenance request handling

--- src/ContosoSuitesWebAPI/Program.cs
+++ src/ContosoSuitesWebAPI/Program.cs
@@ -34,8 +34,18 @@ var builder = WebApplication.CreateBuilder(args);
         apiKey: builder.Configuration["AzureOpenAI:ApiKey"]!
     );
     #pragma warning restore SKEXP0010 // Restore the warning
-     kernelBuilder.Plugins.AddFromType<DatabaseService>();
-     return kernelBuilder.Build();
+    
+    kernelBuilder.Plugins.AddFromType<DatabaseService>();
+    kernelBuilder.Plugins.AddFromType<MaintenanceRequestPlugin>("MaintenanceCopilot");
+    kernelBuilder.Services.AddSingleton<CosmosClient>((_) =>
+        {
+            CosmosClient client = new(
+                connectionString: builder.Configuration["CosmosDB:ConnectionString"]!
+            );
+            return client;
+        });
+
+    return kernelBuilder.Build();
  });
 
 
@@ -161,7 +171,9 @@ app.MapPost("/VectorSearch", async ([FromBody] float[] queryVector, [FromService
 app.MapPost("/MaintenanceCopilotChat", async ([FromBody]string message, [FromServices] MaintenanceCopilot copilot) =>
 {
     // Exercise 5 Task 2 TODO #10: Insert code to call the Chat function on the MaintenanceCopilot. Don't forget to remove the NotImplementedException.
-    throw new NotImplementedException();
+    var response = await copilot.Chat(message);
+    return response;
+
 })
     .WithName("Copilot")
     .WithOpenApi();
